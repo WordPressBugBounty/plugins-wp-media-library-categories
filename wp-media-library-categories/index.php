f25ef25c73191a6bac0a81d8700fcf3868a286d7
Sean Murphy <sean@iamseanmurphy.com>
2025-01-11 09:16:50 +0000

    2.0.2

--- wp-media-library-categories/index.php
+++ wp-media-library-categories/index.php
@@ -3,7 +3,7 @@
  * Plugin Name: Media Library Categories
  * Plugin URI: https://wordpress.org/plugins/wp-media-library-categories/
  * Description: Adds the ability to use categories in the media library.
- * Version: 2.0.1
+ * Version: 2.0.2
  * Author: Jeffrey-WP
  * Text Domain: wp-media-library-categories
  * Domain Path: /languages
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class wpMediaLibraryCategories {
 
-    public $plugin_version = '2.0.1';
+    public $plugin_version = '2.0.2';
 
     /**
      * Initialize the hooks and filters
